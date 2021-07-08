e3d9dcd1fc2ab3c2cffe03d6139c2622edaf2ed8
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2021-07-08 22:00:46 +0300

    Add default categories

--- Data/AuctionHouse.Data/Seeding/CategoriesSeeder.cs
+++ Data/AuctionHouse.Data/Seeding/CategoriesSeeder.cs
@@ -18,6 +18,15 @@
             await dbContext.Categories.AddAsync(new Category { Name = "Travel" });
             await dbContext.Categories.AddAsync(new Category { Name = "Electronics" });
             await dbContext.Categories.AddAsync(new Category { Name = "Jewelry" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Sports" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Gifts" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Health & Fitness" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Services" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Event Tickets" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Theatre" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Art" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Unique" });
+            await dbContext.Categories.AddAsync(new Category { Name = "Apparel" });
 
             await dbContext.SaveChangesAsync();
         }
