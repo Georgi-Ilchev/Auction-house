2f0453483dff2f16b83e8e232bde0a9cd2ba43fc
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2021-08-13 20:30:21 +0300

    Removing 'connection.invoke' in signalR / changing app name

--- AuctionHouse.Common/GlobalConstants.cs
+++ AuctionHouse.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "'Alpha Auction House'";
+        public const string SystemName = "Alpha Auction House";
 
         public const string AdministratorRoleName = "Administrator";
     }
