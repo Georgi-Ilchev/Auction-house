917ef5c6ff65a0cffdb8dd40b2caafa5e2d00dd4
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2021-08-13 15:39:47 +0300

    Add privacy policy / Add app to azure

---
+++ Web/AuctionHouse.Web/obj/Release/net5.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(".alert-dismissible").fadeTo(4000, 500).slideUp(500, function () {
+    $(".alert-dismissible").alert('close');
+});
